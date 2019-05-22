fbc7170649a0d0b710715eaf56d0ac1a3e27449b
sunxuran1995 <215848695@qq.com>
2019-05-22 12:36:42 +0800

    5.22中午,第二天内容结束

--- pinyougou-parent/pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
+++ pinyougou-parent/pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.pinyougou.pojo.TbBrand;
 import entity.PageResult;
+import org.omg.CORBA.PUBLIC_MEMBER;
 
 /**
  * 品牌接口
@@ -15,5 +16,14 @@ public interface BrandService {
 	public List<TbBrand> findAll();
 	//品牌分页
 	public PageResult findPage(int pageNum,int pageSize);
-	
+	//品牌增加
+	public void add(TbBrand brand);
+	//根据id查询实体
+	public TbBrand findOne(Long id);
+	//修改
+	public void update(TbBrand brand);
+	//删除
+	public void delete(Long[] ids);
+	//模糊查询
+	public PageResult findPage(TbBrand brand,int pageNum,int pageSize);
 }
